fc3e39745ece777ac8a1adc6e8728a32575d511f
Newt0507 <leducduong0507@gmail.com>
2024-01-22 21:37:12 +0700

    0122

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private const string IS_WALKING = "IsWalking";
 
+    [SerializeField] private Transform _floorTransform;
     [SerializeField] private float _speed;
 
     private Camera _mainCamera;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     private bool _isWalking;
     private Vector3 _targetPosition;
     private Transform _targetTransform;
+    
 
     private void Awake()
     {
@@ -59,7 +61,7 @@ public class PlayerController : MonoBehaviour
         Ray ray = _mainCamera.ScreenPointToRay(inputPosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            _targetTransform = hit.transform;
+            if(hit.transform != _floorTransform ) _targetTransform = hit.transform;
             _targetPosition = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             _isWalking = true;
         }
@@ -72,14 +74,24 @@ public class PlayerController : MonoBehaviour
 
         if (Vector3.Distance(transform.position, _targetPosition) < 0.1f)
             _isWalking = false;
+
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         if (collision.transform == _targetTransform)
         {
-            Debug.Log(collision.transform);
             _isWalking = false;
+            if (_targetTransform.TryGetComponent(out ClearCounter clearCounter))
+                clearCounter.Interact();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform == _targetTransform)
+        {
+            Debug.Log("Exit!");
         }
     }
 
